b7097f92f1cd2473b6ec8a95ca3871d5843608f2
GACU <gacu@GACU>
2026-04-21 02:14:33 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Booking from "./pages/Booking.jsx";
+import Contact from "./pages/Contact.jsx";
+import Programs from "./pages/Programs.jsx";
+import Tourism from "./pages/Tourism.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
